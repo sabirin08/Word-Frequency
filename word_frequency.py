7f39a1915a07ea57dc8f73c14ec5c3626ef8a5ee
Sabirin Mohamed <sabirin.gca@gmail.com>
2025-02-28 00:12:22 -0500

    word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -13,7 +13,7 @@ def text_word(text):
     
     return sorted_frequency
 
-text = "the bag is here here"
+text = input("Input your text: ")
 print("Word List:", sorted(text_word(text).keys()))
 print("\nBag of Words:")
 for word, count in text_word(text).items():
